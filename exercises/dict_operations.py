f5feed2a33e534010a1b63b5a86a69b6f9fd821e
MCFS436 <596170708@qq.com>
2025-05-01 18:36:22 +0800

    Update dict_operations.py

--- exercises/dict_operations.py
+++ exercises/dict_operations.py
@@ -20,4 +20,13 @@ def student_dict_operations(students_dict, operation, *args):
     - 根据操作返回不同结果
     """
     # 请在下方编写代码
-    pass 
+    if operation == "add":
+        students_dict[args[0]] = args[1]
+    elif operation == "remove":
+        students_dict.pop(args[0], None)
+    elif operation == "update":
+        students_dict[args[0]] = args[1]
+    elif operation == "get":
+        return students_dict[args[0]]
+    return students_dict if operation != "get" else None
+    pass
